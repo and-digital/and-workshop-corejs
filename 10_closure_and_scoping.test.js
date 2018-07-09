a48158d7f82aac7eeaef48fa0e4ccda944b49ad6
Darth Arminius <arminpudic@yahoo.co.uk>
2018-07-09 11:21:42 +0100

    Add describe and replace test with it
    Half the tests were using test and the other half was using it with describe. Tests using test have been changed to use it and describe for consistency.

--- 10_closure_and_scoping.test.js
+++ 10_closure_and_scoping.test.js
@@ -1,54 +1,56 @@
-/*
-    Questions: 
-        - Why is it returning 3 for everything!?
-        - Why would a curried function work here?
-        - Why is it not working? (Related to block / functional scoping) 
-    
-    Task 1) Solve this problem by using currying
-    Task 2) Solve this problem by using .map (It can be done on 1 line :) )
+describe('About closure and scoping', () => {
+  /*
+      Questions:
+          - Why is it returning 3 for everything!?
+          - Why would a curried function work here?
+          - Why is it not working? (Related to block / functional scoping)
 
-*/
+      Task 1) Solve this problem by using currying
+      Task 2) Solve this problem by using .map (It can be done on 1 line :) )
 
-it('should console log a = 2', () => {
-  function test() {
-    console.log(a);
-    console.log(foo());
+  */
 
-    var a = 1;
-    function foo() {
-      return 2;
+  it('should console log a = 2', () => {
+    function test() {
+      console.log(a);
+      console.log(foo());
+
+      var a = 1;
+      function foo() {
+        return 2;
+      }
     }
-  }
 
-  test();
-});
+    test();
+  });
 
-const getFunctions = result => {
-  let funcs = [];
+  const getFunctions = result => {
+    let funcs = [];
 
-  for (let i = 0; i < 3; i++) {
-    funcs[i] = function() {
-      return `I am index ${i}!`;
-    };
-  }
+    for (let i = 0; i < 3; i++) {
+      funcs[i] = function() {
+        return `I am index ${i}!`;
+      };
+    }
 
-  return funcs;
-};
+    return funcs;
+  };
 
-test('First function is 0', () => {
-  const funcs = getFunctions();
+  it('should return index 0 for the first function', () => {
+    const funcs = getFunctions();
 
-  expect(funcs[0]()).toBe('I am index 0!');
-});
+    expect(funcs[0]()).toBe('I am index 0!');
+  });
 
-test('Second function is 1', () => {
-  const funcs = getFunctions();
+  it('should return index 1 for the second function', () => {
+    const funcs = getFunctions();
 
-  expect(funcs[1]()).toBe('I am index 1!');
-});
+    expect(funcs[1]()).toBe('I am index 1!');
+  });
 
-test('Third function is 2', () => {
-  const funcs = getFunctions();
+  it('should return index 2 for the third function', () => {
+    const funcs = getFunctions();
 
-  expect(funcs[2]()).toBe('I am index 2!');
+    expect(funcs[2]()).toBe('I am index 2!');
+  });
 });
