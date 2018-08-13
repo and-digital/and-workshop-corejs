ab6041a10ffc026cc9e559dad95d4faae244caa8
louisbichard <contact@louisjohnbichard.co.uk>
2018-08-13 07:13:56 +0100

    added functions docs

---
+++ src/function.js
@@ -0,0 +1,27 @@
+//  # Function
+
+// A function is the encapsulation of behaviours
+
+// ## Function Signatures
+
+// A function signature is the pattern the function expects you to implement
+// For instance, the following function signature is: 
+// * Expects a single parameter
+// * If no parameter is passed, the assumed default value for the cat is ```Miggins```
+const getAllTheCats = function(name = "Miggins") {
+    return {
+        name
+    }
+};
+
+// TODO: Different types of function definition (fat arrow, regular)
+
+// TODO: Recursive functions
+
+// TODO: Functions that take functions
+
+// TODO: Functions that return functions (currying)
+
+// TODO: Partial application
+
+// TODO: Best practices with functions
