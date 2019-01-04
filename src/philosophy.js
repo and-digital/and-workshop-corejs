c62385aa3290595f5e170e5ac388f66dbc35d7e3
louisbichard <contact@louisjohnbichard.co.uk>
2019-01-04 15:31:02 +0000

    added philosophy

---
+++ src/philosophy.js
@@ -0,0 +1,11 @@
+
+
+// This course is created with an intentional learning philosophy. 
+
+// My motivation comes from: When I was learning I struggled with a lot of concepts. Over time, I pieced together the answers, but they were all over the place and it took years. I wanted to provide as many of those answers in one place, as best as I could. 
+
+// The course has also changed a lot over time. Initially it was very language-focused. Now, I've revised the content to focus heavily on bigger, real-world exmaples that encompass the previous concepts. I believe that will leave a more lasting impression on the learner. I also feel that this type of content is what is often missing in the learning space. The "pulling it all together" part. 
+
+// The content is designed more as a guide than a read-from-start-to-finish linear learning path. I don't believe learning is linear, and I think we learn best from doing. With this in mind, I intend the learner to get to the exercise as soon as possible, and use these real examples as the jumping-off point needed to colour explanations appropriately. 
+
+// But it's the non-linear nature that is why I created the content so that it's as easy to return to as possible. In fact, it helps me to keep refining the content and improving it for my own growth, there's no way someone could grasp it all in one go... unless you like drinking from a firehose! So push the boundaries, ask questions, experiment, re-visit, re-learn, teach others and eventually things will start to fall into place. Just remember that you'll never understand it all, but you should strive to hold the principles. 
