f988f701d90a2b81c6733e54024ff0c0c49b69b8
louisbichard <contact@louisjohnbichard.co.uk>
2019-01-04 14:50:45 +0000

    udpated the function files

---
+++ src/function_basics.js
@@ -0,0 +1,146 @@
+//  # Function
+
+// A function is the encapsulation of behaviours
+
+// Here, we'll discuss probably the most important part of JavaScript: Understanding functions.
+
+// ## Function characteristics
+
+// * Functions can take an inifite number of arguments
+// * A function implicitly returns undefined
+// * A function is a first-class object
+// * A function is an object, with behaviour
+
+// ## Function Signatures
+
+// A function signature is the pattern the function expects you to implement.
+
+// I introduce this word as it's useful when discussing functions, you should constantly think of your function signature. 
+
+// Design by function signature is an essential part of TDD... "How do you want your code to look"
+
+// Where possible: **Design your API upfront**
+
+// Thinking deeply about function signatures and your API's will help you construct more readable code. This is where TDD can also help us. It often makes more sense to define your behaviours and function signatures before you start coding. 
+
+// For instance, the following function signature is: 
+// * Expects a single parameter
+// * If no parameter is passed, the assumed default value for the cat is ```Miggins```
+
+// This is a somewhat trite example, but you want to think deeply about the arguments you expect and the responses you want to send
+
+// Things to consider: 
+// * Should we throw an error if the type is wrong? 
+// * What do we do with additional arguments?
+// * Do we always want to return an object?
+
+const getAllTheCats = function(name = "Miggins") {
+    return {
+        name
+    }
+};
+
+// ## Functions are objects
+
+// Functions in JavaScript are what is known as: _first class objects_.
+
+// That means anything we can do with an object, we can do with a function.
+
+// In this way... functions are like objects but with superpowers.
+
+// Execution of a function is actually a shorthand for it's `call` method ... (but more on this later)
+
+// ## Args vs Params
+
+// What's the difference? 
+
+// Arguments are the values _passed in_ to a function, parameters are what the function _receives_.
+
+// Making the distinction allows us to discuss code more easily. 
+
+// The number of arguments a function requires is called it's _arity_. 
+
+// A function that only takes a single argument is a unary function. 
+
+const unary = x => x;
+
+// **Question:** What is the purpose of the above function? 
+
+// **Answer:** It _drops_ additional arguments, this is useful when passing only a few arguments to a function.
+
+// ## Function definitions vs expressions
+
+// What's the difference between the following: 
+
+try {
+    console.log(iAmADeclaration);
+    console.log(iAmAnExpression);
+} catch(e) {
+    console.log(e);
+}
+
+function iAmADeclaration() {
+
+}
+
+const iAmAnExpression = () => {}
+
+// Only declarations are hoisted, expressions are interpreted when they need to be
+
+// **Question:** Which should you use? 
+
+// **Answer:** Declarations, since they are explicitly named
+
+// ## Anonymous functions
+
+// Functions, when written as a declaration have a `name` property applied
+
+// This name property is then used later in the "call stack"
+
+// Take this declaration:
+
+function iHaveAName() {}
+
+// The name property is set
+console.log(iHaveAName.name);
+console.log(iHaveAName.toString());
+
+// But in the following, we have an `anonymous function` assinged to a variable
+// **Important:** These two _are not the same_.
+const anon = () => {}
+console.log(anon.name);
+
+// **Question:** What will be the output of the following:
+
+const anon2 = function named () {}
+console.log(anon2.name);
+
+// **Answer:** The function is given the function name, not the variable name
+
+// This is the name used in your "call stack"
+
+// ## Passing extra arguments
+
+// Given...
+
+const myFunctionSignature = (a, b) => {
+    return [a, b];
+};
+
+// **Question:** What will happen with the third argument?
+
+console.log(myFunctionSignature(1, 2, 3));
+
+// **Answer:** Nothing, it's lost into the ether. 
+
+// **Note:** No error, but you could throw one if you wanted to ensure the arguments are as you expect...
+
+// ## Function Returning 
+
+// **Question:** What happens if a function has no `return` keyword?
+
+// **Answer:** A function returns `undefined` by default
+
+const undefinedReturn = () => { }
+
+console.log(undefinedReturn());
--- src/functional_methods.js
+++ src/functional_methods.js
@@ -1,28 +1,46 @@
 
 // When performing iterations / loops you can choose between an imperative loop and a declarative functional method
 
+// ## Functional vs Imperative
+
+// What's the difference? 
+
 // **Imperative** = Telling the code what to do and how to do it
 // **Declarative** = Telling the code what to do
 
-// ## Why functional methods are superior
+// ##  Functional methods > Imperative
 
-// Functional methods are immutable
+// Functional methods are immutable. 
 
-// Functional methods utilise closures instead of temporary variables (less chance of pollution)
+// Functional methods utilise closures instead of temporary variables (less chance of pollution).
 
-// **Note:** `.each` is a code smell, it indicates that a function has side effects
+// Functional methods and their style indicate to the reader what type of operation is being performed. 
 
-// TODO explain immutability 
+// A `map` indicates a type of operation, whereas a `for loop` must be read first, before we understand the type of operation and return value. 
 
-// TODO When to choose functional over imperative
+// **Note:** I consider `.each` a code smell, it indicates that a function has side effects. 
 
-// TODO explain .map
+// ## The premise of functional methods
+
+// All functional methods try to adhere to a few rules
+
+// They are performed on the dataset they are given
+
+// They are passed a function, which is then executed on the data set in different ways
+
+// This is a fundamentally different paradigm than most of us are used to as the function passed is executed somewhere else in the code. 
+
+// ## The main functional methods
+
+// The main three functional methods are: `map`, `filter`, and `reduce`. 
 
 // ## .map
 
-// Callback is applied to each item in the collection
+// Callback is applied to each item in the collection.
 
-// Good for use cases, such as modifying a data collection
+// Will return a data structure of the same type and size.
+
+// Good for use cases, such as modifying a data collection.
 
 // Useful if you need a function that returns the collection as the same length
 
@@ -34,8 +52,44 @@ console.log(names);
 
 // ## .filter
 
-// TODO explain .filter
+// Will return a data type of the same or smaller length
+
+// The function is applied to each item, if the function returns true, the item is removed
+
+const numbers = [1, 2, 3, 4, 5];
+const isEven = (val) => val % 2 === 0;
+
+const even = numbers.filter(isEven);
+console.log(even);
+
+const isOdd = (val) => !isEven(val);
+const odd = numbers.filter(isOdd);
+console.log(odd);
+
+// Note how with functional methods we can use re-use our predicate function, this is the real value: Function composition! ❤️
 
 // ## .reduce
 
-// TODO explain .reduce
+// A reduce is fundamentally different as it preserves state as you iterate through your collection
+
+// The return value of the function at each point is passed to the next
+
+// This means you can create an aggregate value
+
+// Sometimes referred to as `fold`
+
+// We take each value and fold it onto / with the next one
+
+const sumEven = numbers.reduce( 
+    
+    // Here, we're checking if it's even
+    (agg, curr) => isEven(curr) 
+        
+    // If it is, we aggregate
+        ? agg + curr 
+        
+        // If not we return the original
+        : agg, 
+0);
+
+console.log(sumEven);
