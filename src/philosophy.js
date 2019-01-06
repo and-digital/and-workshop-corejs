a72be27e03c0f0c1eb21026f46b5fd922b91c9dc
louisbichard <contact@louisjohnbichard.co.uk>
2019-01-06 16:56:28 +0000

    updated training instructions and the philosophy page

--- src/philosophy.js
+++ src/philosophy.js
@@ -8,6 +8,8 @@
 
 // I like to run the content where individuals go through the exercises themselves, but then come together at the end to share their solutions. By doing this, people can see the different considerations of the others and share ideas. 
 
+// One big goal is to try and make the course informative, yet applicable. Informative to understand the _why_ things work the way they do, but also to give as strong a direction and opinion as I can possibly give. To take a stand and say: "Do it this way" with as much authority as I can reasonably muster. 
+
 // The content is designed more as a guide than a read-from-start-to-finish linear learning path. I don't believe learning is linear, and I think we learn best from doing. With this in mind, I intend the learner to get to the exercise as soon as possible, and use these real examples as the jumping-off point needed to colour explanations appropriately. 
 
 // But it's the non-linear nature that is why I created the content so that it's as easy to return to as possible. In fact, it helps me to keep refining the content and improving it for my own growth, there's no way someone could grasp it all in one go... unless you like drinking from a firehose! So push the boundaries, ask questions, experiment, re-visit, re-learn, teach others and eventually things will start to fall into place. Just remember that you'll never understand it all, but you should strive to hold the principles. 
