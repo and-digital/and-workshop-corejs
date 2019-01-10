5a28476eb7492cfb8ae862ccbcb09998711255eb
louisbichard <contact@louisjohnbichard.co.uk>
2019-01-10 09:24:29 +0000

    updated the object oriented example

---
+++ exercises/objectoriented.test.js
@@ -0,0 +1,27 @@
+
+// This is an exercise in design patterns
+
+// Create a calculator object/constructor/class that takes two numbers
+
+// And then exposes the following methods: 
+
+// * `sum`
+// * `multiply`
+
+// For this exercise... _you_ write the tests.
+
+// ## Part 1: Module
+
+// Implement the above using a JS object module.
+
+// ## Part 2: Constructor
+
+// Implement the above using a Constructor
+
+// ## Part 3: Class
+
+// Implement the above using a Class
+
+// ## Part 4: Prototypes
+
+// Modify your above example by using a Class
